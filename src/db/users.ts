653d628668d02f0a1f60b06294a3648e6b74681b
Daniel Florez <danielflorez111@gmail.com>
2025-11-30 12:39:40 +0100

    Adds authentication function

--- src/db/users.ts
+++ src/db/users.ts
@@ -18,9 +18,10 @@ export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne
     'authentication.sessionToken': sessionToken,
 });
 export const getUserById = (id: string) => UserModel.findById(id);
-export const createUser = (values: Record<string, any>) => {
+export const createUser = async (values: Record<string, any>) => {
     const user = new UserModel(values);
-    return user.save().then((user) => user.toObject());
+    await user.save();
+    return user.toObject();
 }
 export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
 export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
